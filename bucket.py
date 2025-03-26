3defb96f8822cd41e184f901388c6a24456d2216
1002Shubham <shubhamkool1002@gmail.com>
2025-03-26 10:11:14 +0530

    Update bucket.py

--- bucket.py
+++ bucket.py
@@ -1,8 +1,10 @@
 import boto3
 
 s3_bucket = boto3.client('s3')
-  response = s3_bucket.create_bucket(
-  bucket_name = 'shubham-boto3-hcl'
+
+response = s3_bucket.create_bucket(
+    Bucket = 'shubham-boto3-hcl'
   )
+
 print(response)
 
